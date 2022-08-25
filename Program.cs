f5117f9279f31c56846383c5bcecb2a3925916e3
Piotr Wisniewski <piowisni@gmail.com>
2022-08-25 16:57:05 +0200

    If-else added

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿int a = 5;
-int b = 6;
+int b = 3;
 if (a + b > 10)
+
     Console.WriteLine("The answer is greater than 10.");
+else
+{
+    Console.WriteLine("The answer is not greater than 10.");    
+}
