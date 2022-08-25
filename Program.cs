4cc97cec1216d17832d0caa5096f3b52ce8b6b48
Piotr Wisniewski <piowisni@gmail.com>
2022-08-25 17:04:16 +0200

    Added OR-AND

--- Program.cs
+++ Program.cs
@@ -1,7 +1,24 @@
-﻿int a = 5;
+﻿/*
+int a = 5;
 int b = 3;
 int c = 4;
 if ((a + b + c > 10) && (a == b))
+{
+    Console.WriteLine("The answer is greater than 10.");
+    Console.WriteLine("And the first number is equal to the second number");
+}
+else
+{
+    Console.WriteLine("The answer is not greater than 10.");
+    Console.WriteLine("And the first number is not equal to the second number");   
+}
+*/
+
+
+int a = 5;
+int b = 3;
+int c = 4;
+if ((a + b + c > 10) || (a == b))
 {
     Console.WriteLine("The answer is greater than 10.");
     Console.WriteLine("Or the first number is equal to the second number");
