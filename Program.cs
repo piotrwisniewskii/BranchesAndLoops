3535b1fbfdf0253b0a4c874babae447347fbfedb
Piotr Wisniewski <piowisni@gmail.com>
2022-08-25 17:00:41 +0200

    Added more complex equation

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿int a = 5;
 int b = 3;
-if (a + b > 10)
-
+int c = 4;
+if ((a + b + c > 10) && (a == b))
+{
     Console.WriteLine("The answer is greater than 10.");
+    Console.WriteLine("Or the first number is equal to the second number");
+}
 else
 {
-    Console.WriteLine("The answer is not greater than 10.");    
+    Console.WriteLine("The answer is not greater than 10.");
+    Console.WriteLine("Or the first number is not equal to the second number");   
 }
